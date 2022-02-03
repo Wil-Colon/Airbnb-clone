2f5fa26723bc54f0afa4d21b2ced98e8709136e5
Your Name <drastic87@gmail.com>
2022-02-02 21:18:48 -0500

    Created Header, lg/md/sm card Components

--- components/Header.js
+++ components/Header.js
@@ -35,7 +35,7 @@ const Header = () => {
             <div className="flex items-center space-x-4 justify-end text-gray-500">
                 <p className="hidden md:inline cursor-pointer">Become a host</p>
                 <GlobeAltIcon className="h-6" />
-                <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+                <div className="flex items-center space-x-2 border-2 p-2 rounded-full cursor-pointer">
                     <MenuIcon className="h-6" />
                     <UserCircleIcon className="h-6" />
                 </div>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head';
 import Banner from '../components/Banner';
 import Header from '../components/Header';
+import SmallCard from '../components/SmallCard';
+import MediumCard from '../components/MediumCard';
+import LargeCard from '../components/LargeCard';
+import Footer from '../components/Footer';
 
-export default function Home() {
+export default function Home({ exploreData, cardsData } = props) {
     return (
         <div className="">
             <Head>
@@ -15,6 +19,54 @@ export default function Home() {
             </Head>
             <Header />
             <Banner />
+            <main className="max-w-7xl mx-auto px-8 sm:px-16">
+                <section className="pt-6">
+                    <h2 className="text-4xl font-semibold pb-5">
+                        Explore Nearby
+                    </h2>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                        {exploreData?.map((exploreData, i) => (
+                            <SmallCard key={i} exploreData={exploreData} />
+                        ))}
+                    </div>
+                </section>
+
+                <section>
+                    <h2 className="text-4xl font-semibold">Live Anywhere</h2>
+                    <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 ml-3">
+                        {cardsData.map((cardData, i) => (
+                            <MediumCard key={i} cardData={cardData} />
+                        ))}
+                    </div>
+                </section>
+                <LargeCard
+                    img="https://links.papareact.com/4cj"
+                    title="The Greatest Outdoors"
+                    description="Wishlists curated by Airbnb"
+                    buttonText="Get Inspired"
+                />
+            </main>
+
+            <Footer />
         </div>
     );
+}
+
+export async function getStaticProps() {
+    try {
+        const exploreData = await fetch('https://links.papareact.com/pyp');
+        const res = await exploreData.json();
+
+        const cardsData = await fetch('https://links.papareact.com/zp1');
+        const res2 = await cardsData.json();
+
+        return {
+            props: {
+                exploreData: res,
+                cardsData: res2,
+            },
+        };
+    } catch (err) {
+        console.error(err);
+    }
 }
